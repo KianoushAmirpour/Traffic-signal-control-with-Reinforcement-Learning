212bbc5762cc171058582724e7c166e465eb72c5
Kian <kianoush.amirpour@gmail.com>
2023-02-20 23:28:18 -0800

    comments added to explain the epsilon schedule

--- utils.py
+++ utils.py
@@ -38,8 +38,8 @@ def sumo_configs(max_step, gui=False):
     return sumo_cmd
 
 def epsilon_schedule(step):
-    progress = (np.clip(step / 540000, a_min=None, a_max=1)) # num of episodes 100 * 5400
-    epsilon = 1 - (1 - 0.01) * progress # 1 is the start value for epsilon and 0.01 is the end value for that
+    progress = (np.clip(step / 540000, a_min=None, a_max=1)) # num of episodes * number of steps per episodes Ex: 100 * 5400, progress goes from 0 to 1
+    epsilon = 1.0 - (1.0 - 0.01) * progress # 1 is the start value for epsilon and 0.01 is the end value for that
     return epsilon
 
 if __name__ == "__main__":
