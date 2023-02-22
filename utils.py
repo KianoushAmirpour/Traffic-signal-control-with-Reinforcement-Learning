13bbb081696102ad1637c46feef2a338189c1f16
Kian <kianoush.amirpour@gmail.com>
2023-02-22 01:15:08 -0800

    two functions added to manage the directories for tracking the experiments

--- utils.py
+++ utils.py
@@ -16,7 +16,8 @@ def seed_everything(seed: int):
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = True
-    
+
+
 def sumo_configs(max_step, gui=False):
     """
     configuration of sumo environment
@@ -37,10 +38,42 @@ def sumo_configs(max_step, gui=False):
 
     return sumo_cmd
 
+
 def epsilon_schedule(step):
     progress = (np.clip(step / 540000, a_min=None, a_max=1)) # num of episodes * number of steps per episodes Ex: 100 * 5400, progress goes from 0 to 1
     epsilon = 1.0 - (1.0 - 0.01) * progress # 1 is the start value for epsilon and 0.01 is the end value for that
     return epsilon
 
+
+def make_dir():
+    """
+    makes needed directories if they don't exist
+    """
+    if not os.path.isdir("checkpoints"):
+        os.makedirs("checkpoints")
+        print("A new directory 'checkpoints' created for saving models\n")
+    if not os.path.isdir("logs"):
+        os.makedirs("logs")
+        print("A new directory 'logs' created for tensorboard\n")
+
+
+def get_dir_name(configs, dir):
+    """create a new folder for each experiment based on configs 
+    """
+
+    experiment_name = '_'.join([str(v) for k, v in configs.items() if k != "device"])
+    if dir == "checkpoints":
+        if os.path.isdir(f"checkpoints/{experiment_name}"):
+            return f"checkpoints/{experiment_name}/"
+        else:
+            os.makedirs(f"checkpoints/{experiment_name}")
+            return f"checkpoints/{experiment_name}/"     
+    else:
+        if os.path.isdir(f"logs/{experiment_name}"):
+            return f"logs/{experiment_name}/"
+        else:
+            os.makedirs(f"logs/{experiment_name}")
+            return f"logs/{experiment_name}/"
+        
 if __name__ == "__main__":
     sumo_configs(100)
